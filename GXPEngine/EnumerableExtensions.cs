96fcc759c5468cdf1916ea84c8987cca07228440
FranticFairy <51452633+FranticFairy@users.noreply.github.com>
2024-02-14 09:35:13 +0100

    Recommit to Work

---
+++ GXPEngine/EnumerableExtensions.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GXPEngine
+{
+    //https://stackoverflow.com/questions/108819/best-way-to-randomize-an-array-with-net
+
+    public static class EnumerableExtensions
+    {
+
+        public static IList<T> Shuffle<T>(this IEnumerable<T> sequence)
+        {
+            return sequence.Shuffle(new Random());
+        }
+
+        public static IList<T> Shuffle<T>(this IEnumerable<T> sequence, Random randomNumberGenerator)
+        {
+            if (sequence == null)
+            {
+                throw new ArgumentNullException("sequence");
+            }
+
+            if (randomNumberGenerator == null)
+            {
+                throw new ArgumentNullException("randomNumberGenerator");
+            }
+
+            T swapTemp;
+            List<T> values = sequence.ToList();
+            int currentlySelecting = values.Count;
+            while (currentlySelecting > 1)
+            {
+                int selectedElement = randomNumberGenerator.Next(currentlySelecting);
+                --currentlySelecting;
+                if (currentlySelecting != selectedElement)
+                {
+                    swapTemp = values[currentlySelecting];
+                    values[currentlySelecting] = values[selectedElement];
+                    values[selectedElement] = swapTemp;
+                }
+            }
+
+            return values;
+        }
+    }
+}
